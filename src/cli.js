4bc7c16be28b0fef5b50df2576083a5d8aa492d1
anakonda <13davidoff@gmail.com>
2021-07-10 16:24:44 +0300

    submit for submissionn

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,7 @@
-import {startServer} from "./server.js";
+import { startServer } from "./server.js";
+
+const PORT = process.env.PORT || 2021;
+const password = process.env.PASSWORD || 'anakonda'
+startServer(PORT, password);
 
 
-const PORT = process.env.PORT || 2021
-startServer(PORT)
--- src/server.js
+++ src/server.js
@@ -2,18 +2,47 @@ import * as http from "http";
 import { networkInterfaces } from "os";
 import { readFile } from "fs/promises";
 import { exec } from "child_process";
-export function startServer(port) {
+let uploadPassword;
+
+export function startServer(port, password) {
   Object.entries(networkInterfaces()).forEach(([name, interfaces]) => {
     interfaces
       .filter((iRecord) => iRecord.family === "IPv4")
       .map((result) => console.log(result.address));
   });
   server.listen(port);
+  uploadPassword = password;
 }
 setTimeout(() => {
   exec('curl "http://localhost:2021/"');
 }, 200);
 const server = http.createServer(async (req, res) => {
+  if (req.method === "POST" && req.url === "/login") {
+    await performLogin(req, res);
+  } else if (req.method === "GET") {
+    await serveContent(req, res);
+  } else {
+    res.statusCode = 400;
+    res.end();
+  }
+});
+
+async function performLogin(req, res) {
+  let body = "";
+  for await (const chunk of req) body += chunk;
+  const formData = new URLSearchParams(body);
+  const userPassword = formData.get("password");
+  if (userPassword === uploadPassword) {
+    res.statusCode = 302;
+    res.setHeader('Location', `${req.headers.referer}`);
+    res.end()
+  } else {
+    res.statusCode = 401;
+    res.end()
+  }
+}
+
+async function serveContent(req, res) {
   const html = await readFile(new URL("./index.html", import.meta.url));
   res.end(html);
-});
+}
